3f715c6075ba8b9a5149e4f325b1cbefbf831393
gonzsanz <davidgonzsanz02@gmail.com>
2023-10-11 20:33:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <VueClock :theme="selectedColor" />
+    <div class="color">
+      <input type="color" v-model="selectedColor">
+    </div>
+  </div>
+</template>
+
+
+<script>
+import VueClock from './components/VueClock.vue'
+export default {
+  components: { VueClock },
+  data() {
+    return {
+      selectedColor: 'red'
+    }
+  },
+}
+
+</script>
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Dancing+Script:wght@500&family=Orbitron:wght@400;800&family=Poppins:wght@400;500;700&display=swap');
+
+body {
+  background-color: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input {
+  cursor: pointer;
+  position: absolute;
+  top: 1rem;
+  left: 30rem;
+}
+</style>
---
+++ src/components/VueClock.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="border">
+        <div class="screen" :style="{ color: theme }">
+            <div class="hours">{{ hour }}</div>
+            <div class="dots">:</div>
+            <div class="minutes">{{ minute }}</div>
+            <div class="dots">:</div>
+            <div class="seconds">{{ second }}</div>
+            <div class="amPm">{{ amPm }}</div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'VueClock',
+    props: {
+        theme: {
+            type: String,
+        }
+    },
+    data() {
+        return {
+            hour: '--',
+            minute: '--',
+            second: '--',
+            amPm: '--'
+        }
+    },
+    mounted() {
+        this.getTime();
+    },
+    methods: {
+        getTime() {
+            setInterval(() => {
+                const date = new Date();
+                this.hour = date.getHours().toString().padStart(2, '0');
+                this.minute = date.getMinutes().toString().padStart(2, '0');
+                this.second = date.getSeconds().toString().padStart(2, '0');
+                this.amPm = this.hour >= 12 ? 'PM' : 'AM';
+            }, 1000);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.border {
+    background-color: #666;
+    width: 600px;
+    height: 200px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+}
+
+.screen {
+    font-family: 'Orbitron', sans-serif;
+    background-color: #000;
+    width: 550px;
+    height: 120px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 70px;
+    box-shadow: 0 0 5px #000;
+}
+
+.amPm {
+    margin: 20px;
+    font-size: 20px;
+}
+</style>
